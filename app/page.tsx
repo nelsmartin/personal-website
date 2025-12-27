3a69bb2d88a15aaeb09d38813d6c85b45f5eeee8
Nels Martin <nelsmartin@Nelss-MacBook-Pro.local>
2025-12-26 16:10:54 -0800

    first draft Willem

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,64 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <main className="flex flex-col items-center space-y-5 mt-10">
+        <h1 className="text-8xl">
+          Willem&apos;s <span className="italic font-bold">Spunky</span> Website
+        </h1>      
+        <h2 className="text-xl max-w-50 text-center">
+          Willem is a sophmore at the University of Montana studying integrated lens-based media. 
+        </h2>
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src="/willemTable.jpeg"
+              alt=""
+              width={1088}
+              height={1088}
+              className="h-100 w-auto"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+
+        <div className="flex flex-row space-x-5">
+          <Image
+              src="/nelsBall.jpeg"
+              alt=""
+              width={4160}
+              height={5157}
+              className="h-96 w-auto"
+            />
+            <Image
+              src="/willemTwo.jpeg"
+              alt=""
+              width={3024}
+              height={4032}
+              className="h-96 w-auto"
+            />
+            
+      </div>
+      <div className="flex flex-row space-x-5">
+<Image
+              src="/willemBeef.jpeg"
+              alt=""
+              width={3024}
+              height={4032}
+              className="h-96 w-auto"
+            />
+      <Image
+              src="/willemBed.jpeg"
+              alt=""
+              width={4032}
+              height={3024}
+              className="h-96 w-auto"
+            />
+      
+
+      </div>
+      
+    
+    </main>
+
+
+
   );
 }
+
